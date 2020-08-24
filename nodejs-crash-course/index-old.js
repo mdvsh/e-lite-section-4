d2cb5ea79010b1267e498bcd9826b4f86fb157ba
Madhav Sharma <shekharmadhav03@gmail.com>
2020-08-24 18:22:12 +0530

    shift learning folders to another repo
    https://github.com/PseudoCodeNerd/learning-express-nodejs-mongoDB

--- nodejs-crash-course/index-old.js
+++
@@ -1,9 +0,0 @@
-const Logger = require("./logger")
-
-console.log('joe mama');
-
-const logger = new Logger()
-
-logger.on('message', data => console.log('listener called', data))
-logger.log('joe  mama')
-logger.log('testing 123')
--- nodejs-crash-course/reference/os_ref.js
+++
@@ -1,6 +0,0 @@
-const os = require('os');
-
-// platform
-console.log(os.platform())
-
-// contd in node REPL not here
